613b8223bce043d7e47c807a7f42332c8c085945
Moksh <moksh13112006@gmail.com>
2025-07-12 13:00:47 +0530

    navbar and basic auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,38 @@
+<<<<<<< HEAD
 import { useState } from 'react'
 import LandingPage from './LandingPage'
 import './App.css'
 import ItemDetails from './ItemDetails'
+<<<<<<< HEAD
 import ItemListing from './ItemListing'
 import AdminPanel from './AdminPanel'
 import UserDashboard from './UserDashboard'
+=======
+=======
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Router, Routes, Route
+import LandingPage from './LandingPage';
+import NavBar from './components/ui/navbar'; 
+import './App.css';
+>>>>>>> c811ff7 (navbar and basic auth)
+>>>>>>> f8a847b (navbar and basic auth)
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
+<<<<<<< HEAD
     <>
       <AdminPanel />
     </>
   )
+=======
+    <Router>
+      <NavBar /> 
+      <Routes> 
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
+>>>>>>> c811ff7 (navbar and basic auth)
 }
 
-export default App
+export default App;
